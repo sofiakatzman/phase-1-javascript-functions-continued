05e4722e4d76d292320d82514b0b802a5206f7c6
Sofia Katzman <katzmansof@gmail.com>
2023-02-25 18:15:56 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+
+let defaultActivity = "roller-skate"
+function saturdayFun(activity = defaultActivity) {
+    return `This Saturday, I want to ${activity}!`;
+  }
+
+let defaultMonday = "go to the office"
+const mondayWork = function(activity = defaultMonday) {
+  return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective (visualFlair = "*") {
+  const result = function(parameter = "special") {
+    return `You are ${visualFlair}${parameter}${visualFlair}!`}
+    return result;
+}
